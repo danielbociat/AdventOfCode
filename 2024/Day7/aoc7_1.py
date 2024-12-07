3039e0d53290327ac9866144ad5af3ab80956b63
danielbociat <danibociat@gmail.com>
2024-12-07 15:09:07 +0100

    Added day 7

---
+++ 2024/Day7/aoc7_1.py
@@ -0,0 +1,24 @@
+import os
+
+script_dir = os.path.dirname(__file__)
+rel_path = "input7.txt"
+abs_file_path = os.path.join(script_dir, rel_path)
+
+sol = 0
+
+def poss(target, curr, i):
+    if i == len(nums):
+        return target == curr
+
+    return poss(target, curr * nums[i], i+1) or poss(target, curr + nums[i], i+1)
+
+with open(abs_file_path) as f:
+    for line in f:
+        lines = line.split(':')
+        target = int(lines[0])
+        nums = [int(el) for el in lines[1].split()]
+
+        if poss(target, nums[0], 1):
+            sol += target
+
+print(sol)
