8a5481dd2c85f6406aa4b4a5e9b3dd67edf2df4d
danielbociat <danibociat@gmail.com>
2024-12-24 18:41:36 +0200

    Added Day24

--- 2024/Day24/aoc24_2.py
+++ 2024/Day24/aoc24_2.py
@@ -1,85 +1,108 @@
 import os
-from collections import defaultdict
 
 script_dir = os.path.dirname(__file__)
 rel_path = "input24.txt"
 abs_file_path = os.path.join(script_dir, rel_path)
 
-with open(abs_file_path) as f:
-    mp = f.read()
+file = open(abs_file_path)
 
-mp = mp.split("\n\n")
+# Parse the input
+for line in file:
+    if line.isspace():
+        break  # End of wire values section
 
-vals = defaultdict(lambda: -1)
-equations = dict()
+formulas = {}
 
-xs = list()
-ys = list()
+# Parse gate formulas
+for line in file:
+    x, op, y, z = line.replace(" -> ", " ").split()
+    formulas[z] = (op, x, y)
 
-for init in mp[0].split('\n'):
-    var_val = init.split(':')
+# Utility function to format wire names
+def make_wire(char, num):
+    return char + str(num).rjust(2, "0")
 
-    vals[var_val[0]] = int(var_val[1])
+# Recursive verification functions
+def verify_z(wire, num):
+    if wire not in formulas:
+        return False
+    op, x, y = formulas[wire]
+    if op != "XOR":
+        return False
+    if num == 0:
+        return sorted([x, y]) == ["x00", "y00"]
+    return (verify_intermediate_xor(x, num) and verify_carry_bit(y, num)) or \
+           (verify_intermediate_xor(y, num) and verify_carry_bit(x, num))
 
-    if var_val[0][0] == 'x':
-        xs.append(var_val[0])
-    else:
-        ys.append(var_val[0])
+def verify_intermediate_xor(wire, num):
+    if wire not in formulas:
+        return False
+    op, x, y = formulas[wire]
+    if op != "XOR":
+        return False
+    return sorted([x, y]) == [make_wire("x", num), make_wire("y", num)]
 
-print(vals)
+def verify_carry_bit(wire, num):
+    if wire not in formulas:
+        return False
+    op, x, y = formulas[wire]
+    if num == 1:
+        if op != "AND":
+            return False
+        return sorted([x, y]) == ["x00", "y00"]
+    if op != "OR":
+        return False
+    return (verify_direct_carry(x, num - 1) and verify_recarry(y, num - 1)) or \
+           (verify_direct_carry(y, num - 1) and verify_recarry(x, num - 1))
 
-zs = list()
+def verify_direct_carry(wire, num):
+    if wire not in formulas:
+        return False
+    op, x, y = formulas[wire]
+    if op != "AND":
+        return False
+    return sorted([x, y]) == [make_wire("x", num), make_wire("y", num)]
 
-for eq in mp[1].split('\n'):
-    eq_vars = eq.split(' ')
+def verify_recarry(wire, num):
+    if wire not in formulas:
+        return False
+    op, x, y = formulas[wire]
+    if op != "AND":
+        return False
+    return (verify_intermediate_xor(x, num) and verify_carry_bit(y, num)) or \
+           (verify_intermediate_xor(y, num) and verify_carry_bit(x, num))
 
-    equations[eq_vars[-1]] = (eq_vars[0], eq_vars[1], eq_vars[2])
-    print(equations[eq_vars[-1]])    
+# Verify a specific wire number
+def verify(num):
+    return verify_z(make_wire("z", num), num)
 
-    if eq_vars[-1][0] == 'z':
-        zs.append(eq_vars[-1])
+# Calculate how far the system progresses
+def progress():
+    i = 0
+    while True:
+        if not verify(i):
+            break
+        i += 1
+    return i
 
-result = ""
+# Identify swapped wires
+swaps = []
 
-zs = sorted(zs)
-print(zs)
+for _ in range(4):  # There are 4 pairs of swapped wires
+    baseline = progress()
+    for x in formulas:
+        for y in formulas:
+            if x == y:
+                continue
+            # Swap the formulas for x and y
+            formulas[x], formulas[y] = formulas[y], formulas[x]
+            if progress() > baseline:  # Check if the swap improves progress
+                break
+            # Restore the original formulas if the swap is not beneficial
+            formulas[x], formulas[y] = formulas[y], formulas[x]
+        else:
+            continue
+        break
+    swaps += [x, y]  # Add the swapped pair to the list
 
-def get_val(key) -> int:
-    if vals[key] != -1:
-        return vals[key]
-    
-    result = -1
-
-    var1,op,var2 = equations[key]
-
-    if op == "AND":
-        result = get_val(var1) & get_val(var2)
-    if op == "OR":
-        result = get_val(var1) | get_val(var2)
-    if op == "XOR":
-        result = get_val(var1) ^ get_val(var2)
-
-    vals[key] = result
-    return result
-
-xVal = ''
-for x in xs:
-    xVal = str(get_val(x)) + xVal
-
-print(xVal)
-
-yVal = ''
-for y in ys:
-    yVal = str(get_val(y)) + yVal
-
-print(yVal)
-
-target = int(xVal, 2) + int(yVal, 2)
-print(target)
-print(bin(target)[2:])
-
-for z in zs:
-    result = str(get_val(z)) + result
-
-print(result)
-print(int(result, 2))
+print(",".join(sorted(swaps)))
